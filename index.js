7b62a9b145786333e7389eb1b0bba50987192b49
Gpcaceres <gpcaceres@espe.edu.ec>
2025-12-10 15:33:40 -0500

    upt-caceres

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb+srv://SrJCBM:bdd2025@cluster0.tjvfmrk.mongodb.net/travel_brain?retryWrites=true&w=majority&appName=Cluster0');
-
-const db=mongoose.connection;
-db.on('error', (error)=> console.error(error));
-db.once('open', () => console.log('System connected to MongoDb Database'));
 app.use(express.json());
 
 const tripRoutes = require('./routes/tripRoutes');
@@ -16,12 +11,9 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/travel-brain/trips', tripRoutes);
 app.use('/travel-brain/users', userRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
 module.exports = app;
 
-mongoose.connect("mongodb+srv://SrJCBM:bdd2025@cluster0.tjvfmrk.mongodb.net/travel_brain")
+mongoose.connect("mongodb+srv://SrJCBM:bdd2025@cluster0.tjvfmrk.mongodb.net/travel_brain?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("Connected to MongoDB - travel_brain");
     app.listen(port, '0.0.0.0', () => {
